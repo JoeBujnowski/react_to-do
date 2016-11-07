e0e17dd84105072869034d3f619248b2b6deeede
Joe Bujnowski <j.bujnowski93@gmail.com>
2016-11-07 12:58:12 -0500

    updated

--- server.js
+++ server.js
@@ -8,33 +8,30 @@ const express    = require('express');
 const bodyParser = require('body-parser');
 const logger     = require('morgan');
 
-const isDev      = !('NODE_ENV' in process.env) && require('dotenv').config() && false;
+// this tests to se if we have NODE_ENV in our environment
+// only load the dotenv if we need it
+const isDev      = !('NODE_ENV' in process.env) && require('dotenv').config() && true;
 const app        = express();
 
 const PORT       = process.argv[2] || process.env.PORT || 3000;
 
-app.use(logger('dev'));
-/*app.use(logger('isDev ? 'dev' : 'common));*/
+app.use(logger(isDev ? 'dev' : 'common'));
 
 /*app.set('views,' '/views');*/
+
+app.use(bodyParser.json());
 
 app.use('/tasks', require('./routes/tasks'));
 app.use('/', require('./routes/index'));
 
 /*app.use((err, req, res, next)) {
-  res.status(500).send('Something broke!').and(next);
+  res.status(500).send('Something broke!').end(next);
 };*/
 
 
-
-
-
-
-
-
-
-
-
 // Keep at bottom of page
-app.listen(port, () => console.log('Server is listening', port));
+
+// Let's go!
+app.listen(PORT);
+/*app.listen(port, () => console.log('Server is listening', port));*/
 
